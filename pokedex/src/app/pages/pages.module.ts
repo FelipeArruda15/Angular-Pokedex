ca0c33b0884a53c71628d84ef71618a10e674c5b
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-11-19 22:16:06 -0300

    CSS styling and module base structure

---
+++ pokedex/src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+// Module Routing
+import { RoutingModule } from './routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule
+  ]
+})
+export class PagesModule { }
