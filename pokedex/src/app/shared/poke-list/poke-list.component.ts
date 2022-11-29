aac1c7ac7f245d61b2c09e17ca3e98e69f3efb7d
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-11-28 22:35:27 -0300

    Função de pesquisa

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -8,15 +8,26 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
 
+  private setAllPokemons: any;
   public getAllPokemons: any;
   constructor(private service: PokeApiService) { }
 
   ngOnInit(): void {
     this.service.apiListAllPokemons.subscribe(
       res => {
-        this.getAllPokemons = res.results
+        this.setAllPokemons = res.results
+        this.getAllPokemons = this.setAllPokemons;
       }
     );
+  }
+
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter( (res: any) =>{
+      return !res.name.indexOf(value.toLowerCase());
+    })
+  
+    this.getAllPokemons = filter;
+
   }
 
 }
